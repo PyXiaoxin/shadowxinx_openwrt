9c4c52c2541555f2312655b0c91a8356c4ff2f16
shadowxinxx <jxxsong@gmail.com>
2021-12-03 11:39:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,5 @@
 #修改主机名
  sed -i 's/OpenWrt/shadowxinx/g' package/base-files/files/bin/config_generate
 
+# 取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
